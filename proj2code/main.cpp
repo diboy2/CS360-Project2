b066fc8f036ee14ceba9c18aaaaa38f891cb7471
Adrian <cagaanan@usc.edu>
2014-10-26 18:10:52 -0700

    Added project 2 files

---
+++ proj2code/main.cpp
@@ -0,0 +1,45 @@
+/*
+NAME: 
+STUDENT ID: 
+*/
+
+#include <cstdlib>
+#include <iostream>
+#include <sys/time.h>
+#include <stdio.h>
+#include "Timer.h"
+
+using namespace std;
+
+void GeneratePuzzle(int nRows, int nColumns, int minVal, int maxVal)
+{
+	// YOUR CODE HERE
+}
+
+int main(int argc, char **argv)
+{
+	// Process the arguments
+	if (argc != 5)
+	{
+		printf("Please specify the number of rows and columns and the minimum and maximum values for each cell (requires 4 parameters)\n");
+		exit(0);
+	}
+	int nRows = atoi(argv[1]);
+	int nColumns = atoi(argv[2]);
+	int minVal = atoi(argv[3]);
+	int maxVal = atoi(argv[4]);
+
+	// Start the timer
+	Timer t;
+	t.StartTimer();
+  	
+	// Generate the puzzle
+	printf("Generating a %dx%d puzzle with values in range [%d-%d]\n", nRows, nColumns, minVal, maxVal);
+	
+	GeneratePuzzle(nRows, nColumns, minVal, maxVal);
+		
+	// Print the elapsed time
+    printf("Total time: %.6lf seconds\n", t.GetElapsedTime());
+
+	return 0;
+}
